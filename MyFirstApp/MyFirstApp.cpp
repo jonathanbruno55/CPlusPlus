ba2338509d14c6c3036dfca11f038ea33e5d46e2
Jonathan Bruno <52010584+jonathanbruno55@users.noreply.github.com>
2021-03-02 02:22:28 -0600

    Add files via upload

---
+++ MyFirstApp/MyFirstApp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";//this will output to the console hello world
+    cout << "My name is Jonathan."<<endl;
+    cout << "Today is Friday";
+    cout << "Have a nice day";
+    system("pause>0");
+}
